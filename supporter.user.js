6f648f7e7156977caedf94659f6822862a69f447
rustygutu <102919613+rustygutu@users.noreply.github.com>
2022-04-03 18:59:26 +0100

    Update supporter.user.js

--- supporter.user.js
+++ supporter.user.js
@@ -11,8 +11,8 @@
 // @downloadURL  https://rustygutu.github.io/rplace/supporter.user.js
 // ==/UserScript==
 
-const X_OFFSET = 773
-const Y_OFFSET = 735
+const X_OFFSET = 1808
+const Y_OFFSET = 615
 
 async function run() {
     let run = false
@@ -150,7 +150,7 @@ async function run() {
                 resolve({template_ctx: template_ctx, template_img: img})
             }
             img.onerror = reject
-            img.src = "https://rustygutu.github.io/saka-temp2.png?tstamp=" + Math.floor(Date.now() / 10000);
+            img.src = "https://rustygutu.github.io/saka-temp2.png";
         })
     }
 
